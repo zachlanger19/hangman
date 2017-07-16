2448b96ca416b629f55fe826400af4a7dcfb1c8c
zachlanger19 <zachlanger19@gmail.com>
2017-07-16 04:26:12 -0700

    Intelligent guessing

--- hangmanPlayerIntuitive.rb
+++ hangmanPlayerIntuitive.rb
@@ -1,72 +1,88 @@
 #! /usr/bin/env ruby
 
-words = Array.new
-File.open("words.txt").each do |line|
+words = []
+File.open('wordsSortedIntuitively.txt').each do |line|
   word = line.chomp.to_s
   words.insert(-1, word)
 end
 
+guessed_letters = []
+number_of_errors = 0
+length_indexes = [0, 26, 445, 2_476, 9_204, 24_051, 52_178, 91_873,
+                  140_877, 191_532, 234_952, 270_564, 298_233, 318_204, 331_776,
+                  340_300, 345_332, 348_229, 349_674, 350_423, 350_778, 350_945,
+                  351_018, 351_048, 351_060, 351_068, 351_071, 351_073, 351_075]
+
 puts 'HANGMAN'
 puts 'Think of a word'
 puts 'How many letters is it?'
-wordLength = gets.chomp.to_i
-letters = ('a'..'z').to_a
+word_length = gets.chomp.to_i
 
-counter = 0
-while counter < words.length
-  if words[counter].length != wordLength
-    words.delete_at(counter)
-  else
-    print '.'
-    counter += 1
+# Get words that match desired length
+start_index = length_indexes[word_length - 1]
+end_index = length_indexes[word_length] - 1
+words = words[start_index..end_index]
+puts "There are #{words.length} possibilities"
+
+def pick_letter(words, guessed_letters)
+  # Create initial hash of available letters
+  letters = {}
+  ('a'..'z').each do |letter|
+    letters[letter] = 0
   end
+
+  # Count occurrences
+  words.each do |word|
+    word.split('').each do |occurred_letter|
+      letters[occurred_letter] += 1
+    end
+  end
+
+  # Find best letter
+  picked_letter = ''
+  picked_number_of_occurrences = 0
+  letters.each do |letter, occurrences|
+    if !guessed_letters.include?(letter) && occurrences > picked_number_of_occurrences
+      picked_letter = letter
+      picked_number_of_occurrences = occurrences
+    end
+  end
+
+  picked_letter
 end
 
-def Game(lettersToGuess = [], possibleWords = [])
-  guessIndex = rand(lettersToGuess.length)
-  puts "Is there a/an #{lettersToGuess[guessIndex]}?"
-  present = gets.chomp
-  if present == 'y'
+# Main
+until words.length <= 1 || number_of_errors == 7
+  guessed_letter = pick_letter(words, guessed_letters)
+  guessed_letters.push(guessed_letter)
+  puts "Is there a/an #{guessed_letter}?"
+  responce = gets.chomp
+  # Keep words that meet specifications
+  if responce[0] == 'y'
     puts 'How many times does it occur?'
-    occurrences = gets.chomp.to_i
-    occurrences.times do
-      puts "Where's the letter located?"
-      answerIndex = gets.chomp.to_i - 1
-      counter = 0
-      while counter < possibleWords.length
-        if possibleWords[counter][answerIndex] != lettersToGuess[guessIndex]
-          possibleWords.delete_at(counter)
-        else
-          counter += 1
-        end
-      end
-    end
-    counter = 0
-    while counter < possibleWords.length
-      if possibleWords[counter].count(lettersToGuess[guessIndex]) != occurrences
-        possibleWords.delete_at(counter)
-      else
-        counter += 1
-      end
+    number_of_occurrences = gets.chomp.to_i
+    number_of_occurrences.times do
+      puts 'Which index is it?'
+      index = gets.chomp.to_i
+      words.select! { |word| word[index] == guessed_letter }
     end
   else
-    counter = 0
-    while counter < possibleWords.length
-      if possibleWords[counter].include?(lettersToGuess[guessIndex])
-        possibleWords.delete_at(counter)
-      else
-        counter += 1
-      end
-    end
+    # Remove words that contain the letter
+    words.reject! { |word| word.include?(guessed_letter) }
+    number_of_errors += 1
   end
-  if possibleWords.length == 1 || possibleWords.length == 0
-    puts possibleWords
-  else
-    puts possibleWords
-    puts "#{possibleWords.length} possible words"
-    lettersToGuess.delete_at(guessIndex)
-    Game(lettersToGuess, possibleWords)
-  end
+
+  puts
+  puts "Errors: #{number_of_errors}"
+  puts "There are #{words.length} possibilities"
 end
 
-Game(letters, words)
+if number_of_errors == 7
+  puts 'You win :('
+  puts 'Remaining possibilities:'
+  puts words
+elsif words.length.zero?
+  puts 'There must be some mistake :/'
+else
+  puts "It's #{words[0]}!!!"
+end
--- test.rb
+++ test.rb
@@ -1,13 +1,16 @@
-words = Array.new
-File.open('wordsSortedIntuitively.txt').each do |line|
-  word = line.chomp.to_s
-  words.insert(-1, word)
-end
+# words = Array.new
+# File.open('wordsSortedIntuitively.txt').each do |line|
+#   word = line.chomp.to_s
+#   words.insert(-1, word)
+# end
+#
+# previous_length = 0
+# words.each_with_index do |word, index|
+#   if word.length > previous_length
+#     print index.to_s + ', '
+#     previous_length = word.length
+#   end
+# end
 
-previous_length = 0
-words.each_with_index do |word, index|
-  if word.length > previous_length
-    puts index
-    previous_length = word.length
-  end
-end
+arr = ['hello', 'world']
+puts arr.count('hello')
